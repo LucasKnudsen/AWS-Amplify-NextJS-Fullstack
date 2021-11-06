1acaa2a347b13a962c621d0e8d91190a9cb4fe1c
lucas <laksteelhouse@gmail.com>
2021-11-06 06:39:53 +0100

    Initial setup

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import { Typography } from '@mui/material'
+
+const Home = () => {
+  return (
+    <div>
+      <Typography variant='h1'>Hello World</Typography>
+    </div>
+  )
+}
+
+export default Home
